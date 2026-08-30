f2c8b6d485716559083cf18207abe090bedb02c6
Tushar Adhatrao <tusharadhatrao@gmail.com>
2026-08-30 20:16:38 +0530

    feat: replace generic example structure with Ollama simple implementation and update build configuration

---
+++ examples/src/Ollama/Simple.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Ollama.Simple (runApp) where
+
+import Control.Monad.Except (runExceptT)
+import Langchain.Provider.Ollama
+import Langchain.Prelude
+import qualified Data.Text.IO as T
+
+runApp :: IO ()
+runApp = do 
+    o <- newOllama "qwen3.5:9b"
+    let msg = [userMessage "Write a poem about functional programming"]
+    res <- runExceptT $ invoke o msg Nothing
+    case res of
+      Left err -> T.putStrLn $ errorMessage err
+      Right m -> T.putStrLn $ extractMessageText m
