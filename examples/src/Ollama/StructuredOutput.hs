0941603bf80bccd94e7dc2060c12bc1c7d465152
Tushar Adhatrao <tusharadhatrao@gmail.com>
2026-09-05 14:06:20 +0530

    feat: add structured output example with new jsonollama executable

---
+++ examples/src/Ollama/StructuredOutput.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeApplications #-}
+module Ollama.StructuredOutput (runApp) where
+
+import Langchain.Prelude
+import Langchain.Provider.Ollama
+import qualified Data.Text.IO as T
+import qualified Data.Text as T
+import Data.Aeson
+import GHC.Generics
+
+data Person = Person {
+            name :: T.Text,
+            age :: Int,
+            location :: T.Text
+        } deriving (Show, Eq, Generic, FromJSON, ToSchema)
+
+inputPrompt :: T.Text
+inputPrompt = T.unlines [
+        "For the given below information, extract information about Jesse.",
+        "the 24 year old Jesse was staying in New York due to his work."
+    ]
+
+runApp :: IO ()
+runApp = do
+    o <- newOllama "gemma3"
+    let msg = [ userMessage inputPrompt ]
+    let chatReq = withStructuredOutput @Person (chatRequestFor o msg)
+    res <- runLangchainTIO $ do
+        invoke o msg (Just chatReq)
+    case res of
+      Left err -> T.putStrLn $ errorMessage err
+      Right r -> T.putStrLn $ extractMessageText r
