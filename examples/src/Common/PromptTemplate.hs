9eb4832b0d29356f351fd7a10846999320804d7b
Tushar Adhatrao <tusharadhatrao@gmail.com>
2026-09-06 15:56:57 +0530

    feat: add RAG and prompt template examples and associated CLI executables

---
+++ examples/src/Common/PromptTemplate.hs
@@ -0,0 +1,8 @@
+module Common.PromptTemplate (runApp) where
+
+import Langchain.PromptTemplate.String
+
+runApp :: IO ()
+runApp = do
+    let _ = renderFStringTemplate
+    undefined
--- examples/src/Ollama/Simple.hs
+++ examples/src/Ollama/Simple.hs
@@ -4,38 +4,41 @@ module Ollama.Simple (runApp) where
 
 import Control.Monad.Except (runExceptT)
 import qualified Data.Text.IO as T
+import qualified Data.Text.Lazy as T
 import Langchain.Prelude
+import Langchain.PromptTemplate.Prompt
+import qualified Data.Map as Map
+
+runPromptTemplateExample :: IO ()
+runPromptTemplateExample = do
+    let items = Map.fromList [("name", "John"), ("age", "25")]
+    let inputText = "{name} is of age of {age}, he is only {age}!"
+    case renderFStringTemplate items inputText of
+      Left err -> T.putStrLn $ errorMessage err
+      Right r -> T.putStrLn r
 
 runApp :: IO ()
 runApp = do
+  runPromptTemplateExample
+  let prompt = "Write a poem about functional programming"
+  let splittedChars = splitTextRecursive defaultRecursiveCharacterSplitterOps prompt
+  mapM_ (T.putStrLn . T.toStrict) splittedChars
   o <- newOllama "gemma3" defaultConfig
-  let msg = [userMessage "Write a poem about functional programming"]
+  let msg = map (userMessage . T.toStrict) [prompt]
   res <- runExceptT $ invoke o msg Nothing
   case res of
     Left err -> T.putStrLn $ errorMessage err
     Right m -> T.putStrLn $ extractMessageText m
 
-  -- Example showcasing passing chatOptions (temperature, topP, numCtx) per request using withOptions and defaultOptions:
   let reqWithOptions =
         withOptions
-          (defaultOptions {optTemperature = Just 0.7, optTopP = Just 0.9, optNumCtx = Just 4096})
+          (defaultOptions {optTemperature = Just 0.7, optNumCtx = Just 4096})
           (chatRequestFor o msg)
   resWithOptions <- runExceptT $ invoke o msg (Just reqWithOptions)
   case resWithOptions of
     Left err -> T.putStrLn $ errorMessage err
     Right m -> T.putStrLn $ extractMessageText m
 
-  -- Example showcasing custom chatOptions per request:
-  let req =
-        withOptions
-          (defaultOptions {optTemperature = Just 0.2, optNumCtx = Just 2048})
-          (chatRequestFor o msg)
-  resWithReq <- runExceptT $ invoke o msg (Just req)
-  case resWithReq of
-    Left err -> T.putStrLn $ errorMessage err
-    Right m -> T.putStrLn $ extractMessageText m
-
-  -- another example showcasing batch usage.
   let ques =
         [ "What is Functor in Haskell?"
         , "What is applicative in Haskell?"
