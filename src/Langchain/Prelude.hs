e6aca1aa4905c8f93a9cd53cf1da125a4406fd4f
Tushar Adhatrao <tusharadhatrao@gmail.com>
2026-09-12 17:10:07 +0530

    chore: update stack resolver, streamline README, and restrict OpenAIEmbeddings constructor export

--- src/Langchain/Prelude.hs
+++ src/Langchain/Prelude.hs
@@ -227,7 +227,7 @@ module Langchain.Prelude
     -- * Embeddings
   , Embeddings (..)
   , OllamaEmbeddings (..)
-  , OpenAIEmbeddings (..)
+  , OpenAIEmbeddings (OpenAIEmbeddings)
   , defaultOpenAIEmbeddings
   , textEmbedding3Small
   , textEmbedding3Large
@@ -386,7 +386,7 @@ import Langchain.DocumentLoader.WebPage
 import Langchain.Embeddings.Core
 import Langchain.Embeddings.Ollama (OllamaEmbeddings (..))
 import Langchain.Embeddings.OpenAI
-  ( OpenAIEmbeddings (..)
+  ( OpenAIEmbeddings (OpenAIEmbeddings)
   , defaultOpenAIEmbeddings
   , textEmbedding3Large
   , textEmbedding3Small
